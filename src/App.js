95516dad4d0c250cba23481b6bee7d1e4f1369b6
MBecerra777 <marcelo.becerra@pucp.edu.pe>
2021-10-07 18:05:10 -0500

    Footer
    Se agrega el footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Ciclo from './Components/Ciclo';
 import Navbar from './Components/Navbar/Navbar'
 import NavbarUser from './Components/NavbarUser/NavbarUser'
 import fotoUsuario from './assets/Images/profile-photo.png'
+import Footer from './Components/Footer/Footer';
 
 function App() {
 
@@ -16,8 +17,8 @@ function App() {
       <Navbar/>
       <NavbarUser nombre = {nombre} rol = {rol} foto = {fotoUsuario}/>
       <Ciclo />
-      Hola, soy E
 
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import facebook from "../../assets/Images/facebook.png";
+import github from "../../assets/Images/github.png";
+import linkedin from "../../assets/Images/linkedin.png";
+import twitter from "../../assets/Images/twitter.png";
+import whatsapp from "../../assets/Images/whatsapp.png";
+import './Footer.css';
+
+const Footer = () =>{
+    return(
+        <div className = "main-footer">
+            <nav className = "container">
+                <img className = "fb"   src={facebook} alt=""/>
+                <img className = "gb" src={github} alt=""/>
+                <img className = "lk" src={linkedin} alt=""/>
+                <img className = "tw" src={twitter} alt=""/>
+                <img className = "wpp" src={whatsapp} alt=""/>
+            </nav>
+        </div>
+    )
+}
+
+export default Footer;
