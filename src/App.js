8c30f4a730c462a2085d456a3bba4796f7c56cb6
Lucas Podestá <carsino12@gmail.com>
2021-10-07 19:33:31 -0500

    Segundo commit
    Commit secundario

--- src/App.js
+++ src/App.js
@@ -3,19 +3,6 @@ import { lightBlue } from '@mui/material/colors';
 import { typography } from '@mui/system';
 import Ciclo from './Components/Ciclo';
 
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#fefefe'
-    },
-    secondary: lightBlue
-  },
-  typography: {
-    fontFamily: 'Quicksand',
-    fontWeightMedium:600,
-  }
-})
 function App() {
   return (
     <div className="App">
--- src/Services/Ciclo.js
+++ src/Services/Ciclo.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 export const getAllCiclos = async () => {
-
+  
   try {
     const response = await axios.get("http://52.200.33.206:8080/ciclo/");
     console.log("Promesa - GET de todos los Ciclos")
     const { data } = response;
-    return data;
+    console.log(data);
+    return data;  
   }
   catch (error) {
     console.log(error.message);
@@ -16,7 +17,7 @@ export const getAllCiclos = async () => {
 
 export const addCiclo = async (newObject) => {
   try {
-    const response = await axios.post("http://52.200.33.206:8080/ciclo/", newObject);
+    const response = await axios.post("http://52.200.33.206:8080/ciclo/", newObject); 
     //const { data } = response;
     console.log(response.data);
     return (response.data)
